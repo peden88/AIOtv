0c30b83e6573ab75c69d11d7f647bd3f31616f37
peden88 <76457061+peden88@users.noreply.github.com>
2026-08-28 16:55:22 +0100

    fix(aiotv): use RowScope weight extension

--- app/src/main/java/com/nuvio/tv/ui/screens/addon/AddonManagerScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/addon/AddonManagerScreen.kt
@@ -2,7 +2,6 @@ package com.nuvio.tv.ui.screens.addon
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
